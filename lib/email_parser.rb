688530466b5439331668be79d4549985fd0906fc
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-12-16 23:40:57 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  attr_accessor :name
+  attr_accessor :parse
   def initialize(parse)
     @name = parse
   end
