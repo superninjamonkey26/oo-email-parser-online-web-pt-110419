076db4ab27d92384928160f9de711592a2909450
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-12-17 00:48:41 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,6 +11,6 @@ class EmailAddressParser
   end
   def parse
     puts splitEmail = emails.split.collect {|address| address.split(" ")}
-    returnArr = splitEmail
+    returnArr = splitEmail.flatten.uniq
   end
 end
